883ed39e047173d5ba668ad480461a06f6f82ea8
xiaolonghao <xiaolonghao@linklogis.com>
2022-03-30 16:18:03 +0800

    upd: 新增两道对象隐式类型转换自测题

--- JS高级/对象的认知升级/04.隐式类型转换.js
+++ JS高级/对象的认知升级/04.隐式类型转换.js
@@ -30,3 +30,47 @@ console.log('keys:', Object.keys(obj)) // 20 , value , toString , valueOf
 console.log('${obj}:', `${obj}`) // 20 ，期望是 string ，所以调用的是 obj.toString
 console.log('obj + 1:', obj + 1) // 11 ，期望是 number ，所以调用的是 obj.valueOf
 console.log('obj + ""', obj + '') // 10 ，期望是 number ，所以调用的是 obj.valueOf
+
+// 学习自检
+// const obj = {}
+// const objA = { propertyA: 'A' },
+//   objB = { propertyB: 'B' }
+// obj[objA] = 'objectA'
+// obj[objB] = 'objectB'
+
+// /**
+//  * 考察点：
+//  *  + 对象的属性key只能是 string
+//  *  + 隐式类型转换：objA 和 objB 转换为原始值都是字符串 '[object Object]'
+//  */
+// for (let [k, v] of Object.entries(obj)) {
+//   console.log('k:', k, ', v:', v) // k: [object, Object], v: objectB
+// }
+
+// 加点难度
+// const obj = {}
+// const objA = {
+//     propertyA: 'A',
+//     toString() {
+//       return 'objA'
+//     },
+//   },
+//   objB = {
+//     propertyB: 'B',
+//     valueOf() {
+//       return 'objB'
+//     },
+//   }
+// obj[objA] = 'objectA'
+// obj[objB] = 'objectB'
+
+// /**
+//  * 考察点：
+//  *  + 对象的属性key只能是 string
+//  *  + 隐式类型转换：
+//  *    - objA 转换为原始值是字符串 'objA'
+//  *    - objB 转换为原始值是字符串 '[object Object]'，这里很容易错误的认为是 'objB'，当期望是 string 时，如果私有属性没有 toString 方法，会去找原型上的 toString 方法，而不是去找私有的 valueOf 方法
+//  */
+// for (let [k, v] of Object.entries(obj)) {
+//   console.log('k:', k, ', v:', v) // k: [object Object] , v: objectB ; k: [object, Object], v: objectB
+// }
